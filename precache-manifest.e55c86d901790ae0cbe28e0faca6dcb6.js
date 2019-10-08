ca26124b0a4ee38121f1072233221c01f16cbeb5
Aleksander <aleksander@MacBook-Air-Aleksander.local>
2019-10-08 18:13:07 +0200

    Updates

---
+++ precache-manifest.e55c86d901790ae0cbe28e0faca6dcb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e00e613f4c612e7758a99dc066f0e53",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "eb92a759bc6e6bab6065",
+    "url": "/quiz/static/css/main.6f871de0.chunk.css"
+  },
+  {
+    "revision": "831d27e94b3fa992735b",
+    "url": "/quiz/static/js/2.5c1448ca.chunk.js"
+  },
+  {
+    "revision": "eb92a759bc6e6bab6065",
+    "url": "/quiz/static/js/main.3a1ca735.chunk.js"
+  },
+  {
+    "revision": "3a6da02e950c25ae9c15",
+    "url": "/quiz/static/js/runtime-main.e0b783b5.js"
+  }
+]);
